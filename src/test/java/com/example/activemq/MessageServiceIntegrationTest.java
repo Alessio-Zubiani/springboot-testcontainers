6b11a8bf8f4aa799859c85460a46680a29623822
alessiozubiani <alessio.zubiani1996@gmail.com>
2024-05-16 18:49:16 +0200

    Added integration tests

--- src/test/java/com/example/activemq/MessageServiceIntegrationTest.java
+++ src/test/java/com/example/activemq/MessageServiceIntegrationTest.java
@@ -9,13 +9,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import jakarta.jms.JMSException;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = { JmsConfiguration.class })
+@ContextConfiguration(classes = { JmsConfiguration.class })
 class MessageServiceIntegrationTest {
 	
 	@Autowired
