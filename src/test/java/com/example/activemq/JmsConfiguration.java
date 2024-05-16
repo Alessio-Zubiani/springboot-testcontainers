cc14186971aa742f9d8136faed3d954950a7e4eb
alessiozubiani <alessio.zubiani1996@gmail.com>
2024-05-16 18:27:20 +0200

    Added integration tests

--- src/test/java/com/example/activemq/JmsConfiguration.java
+++ src/test/java/com/example/activemq/JmsConfiguration.java
@@ -37,34 +37,3 @@ public class JmsConfiguration {
     }
 
 }
-
-/*@SpringBootTest
-@Testcontainers
-public class JmsTest {
-
-	@Container
-    static GenericContainer<?> artemis = new GenericContainer<>(DockerImageName.parse("apache/activemq-artemis:latest-alpine"))
-            .withEnv("ANONYMOUS_LOGIN", "true")
-            .withExposedPorts(61616);
-
-    @DynamicPropertySource
-    static void artemisProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.artemis.broker-url", () -> "tcp://%s:%d".formatted(artemis.getHost(), artemis.getMappedPort(61616)));
-    }
-
-    @Autowired
-    private JmsTemplate jmsTemplate;
-
-    @Test
-    void sendMessage() throws JMSException {
-        this.jmsTemplate.convertAndSend("test.queue", "Hello, JMS!");
-
-        Message message = this.jmsTemplate.receive("testQueue");
-        assertThat(message).isInstanceOf(TextMessage.class);
-        
-        TextMessage textMessage = (TextMessage) message;
-        assertThat(textMessage.getText()).isEqualTo("Hello, JMS!");
-    }
-
-}
-*/
