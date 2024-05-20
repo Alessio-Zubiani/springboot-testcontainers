5f88dce9e680aad5654979ea4268bac4a51c4abd
alessiozubiani <alessio.zubiani1996@gmail.com>
2024-05-20 11:59:36 +0200

    Aggiunti IT ed E2E tests

--- src/test/java/com/example/activemq/EmployeeControllerEndToEndTest.java
+++ src/test/java/com/example/activemq/EmployeeControllerEndToEndTest.java
@@ -1,4 +1,4 @@
-/*package com.example.activemq;
+package com.example.activemq;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,4 +27,3 @@ class EmployeeControllerEndToEndTest {
 	}
 
 }
-*/
