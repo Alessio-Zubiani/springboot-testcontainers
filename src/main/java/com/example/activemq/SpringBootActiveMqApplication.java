f691515fd11c0474ad4ab4d665607f10768ddc71
alessiozubiani <alessio.zubiani1996@gmail.com>
2024-05-20 16:49:00 +0200

    Aggiunti IT ed E2E tests

--- src/main/java/com/example/activemq/SpringBootActiveMqApplication.java
+++ src/main/java/com/example/activemq/SpringBootActiveMqApplication.java
@@ -2,7 +2,10 @@ package com.example.activemq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,5 +16,10 @@ public class SpringBootActiveMqApplication extends SpringBootServletInitializer 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootActiveMqApplication.class, args);
 	}
+	
+	@Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
 
 }
