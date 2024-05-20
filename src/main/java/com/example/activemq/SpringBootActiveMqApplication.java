8aecfa920fb8f9d34d7e6894564023f506741690
alessiozubiani <alessio.zubiani1996@gmail.com>
2024-05-20 17:00:16 +0200

    Aggiunti IT ed E2E tests

--- src/main/java/com/example/activemq/SpringBootActiveMqApplication.java
+++ src/main/java/com/example/activemq/SpringBootActiveMqApplication.java
@@ -2,10 +2,7 @@ package com.example.activemq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,10 +13,5 @@ public class SpringBootActiveMqApplication extends SpringBootServletInitializer 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootActiveMqApplication.class, args);
 	}
-	
-	@Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
-    }
 
 }
--- src/test/java/com/example/activemq/EmployeeControllerEndToEndTest.java
+++ src/test/java/com/example/activemq/EmployeeControllerEndToEndTest.java
@@ -7,19 +7,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.example.activemq.controller.EmployeeController;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 
-@SpringBootTest(classes = { SpringBootActiveMqApplication.class }, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
-//@WebMvcTest(EmployeeController.class)
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(EmployeeController.class)
 @ContextConfiguration(classes = { JdbcConfiguration.class })
 class EmployeeControllerEndToEndTest {	
 	
