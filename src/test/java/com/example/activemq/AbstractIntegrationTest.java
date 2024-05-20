46e6a44f6d328651d2213da44b3ec7e949a86b8e
alessiozubiani <alessio.zubiani1996@gmail.com>
2024-05-20 14:55:10 +0200

    Aggiunti IT ed E2E tests

--- src/test/java/com/example/activemq/AbstractIntegrationTest.java
+++ src/test/java/com/example/activemq/AbstractIntegrationTest.java
@@ -1,14 +1,25 @@
 package com.example.activemq;
 
 import java.time.Duration;
+import java.util.HashMap;
 
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.oracle.OracleContainer;
 import org.testcontainers.utility.DockerImageName;
 
+import jakarta.persistence.EntityManagerFactory;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -34,5 +45,36 @@ public abstract class AbstractIntegrationTest {
         registry.add("spring.datasource.username", () -> oracleContainer.getUsername());
         registry.add("spring.datasource.password", () -> oracleContainer.getPassword());
     }
+	
+	@Bean
+    public DataSource dataSource() {
+		
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("oracle.jdbc.OracleDriver");
+        dataSourceBuilder.url(oracleContainer.getJdbcUrl());
+        dataSourceBuilder.username(oracleContainer.getUsername());
+        dataSourceBuilder.password(oracleContainer.getPassword());
+        
+        return dataSourceBuilder.build();
+    }
+	
+	@Bean
+	public EntityManagerFactoryBuilder entityManagerFactoryBuilder() {
+		return new EntityManagerFactoryBuilder(new HibernateJpaVendorAdapter(), new HashMap<>(), null);
+	}
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManager(EntityManagerFactoryBuilder entityManagerFactoryBuilder, DataSource dataSource) {
+		
+		return entityManagerFactoryBuilder
+				.dataSource(dataSource)
+				.packages("com.example.activemq.service")
+				.build();
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory entityManager) {
+		return new JpaTransactionManager(entityManager);
+	}
 
 }
