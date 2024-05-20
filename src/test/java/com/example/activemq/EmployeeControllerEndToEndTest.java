fe1d3bd89cc7d807cd1660af711520624a54f1b2
alessiozubiani <alessio.zubiani1996@gmail.com>
2024-05-20 16:07:50 +0200

    Aggiunti IT ed E2E tests

--- src/test/java/com/example/activemq/EmployeeControllerEndToEndTest.java
+++ src/test/java/com/example/activemq/EmployeeControllerEndToEndTest.java
@@ -1,6 +1,7 @@
 package com.example.activemq;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.example.activemq.controller.EmployeeController;
@@ -8,6 +9,7 @@ import com.example.activemq.controller.EmployeeController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
@@ -26,7 +28,8 @@ class EmployeeControllerEndToEndTest {
 	@Test
 	void testGetAllEmployees() throws Exception {
 		
-		this.mockMvc.perform(get("/api/v1/employees"))
+		this.mockMvc.perform(get("/api/v1/employees").accept(MediaType.APPLICATION_JSON))
+        	.andDo(print())
 			.andExpect(status().isOk());
 	}
 
