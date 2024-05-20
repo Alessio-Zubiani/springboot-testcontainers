6725df00127444cb65d5b7f473e41968ac6902c3
alessiozubiani <alessio.zubiani1996@gmail.com>
2024-05-20 16:52:46 +0200

    Aggiunti IT ed E2E tests

--- src/test/java/com/example/activemq/EmployeeControllerEndToEndTest.java
+++ src/test/java/com/example/activemq/EmployeeControllerEndToEndTest.java
@@ -9,15 +9,13 @@ import com.example.activemq.controller.EmployeeController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
 
-@SpringBootTest(classes = { SpringBootActiveMqApplication.class }, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@WebMvcTest(EmployeeController.class)
+@WebMvcTest(EmployeeController.class)
 @ContextConfiguration(classes = { JdbcConfiguration.class })
 class EmployeeControllerEndToEndTest {	
 	
