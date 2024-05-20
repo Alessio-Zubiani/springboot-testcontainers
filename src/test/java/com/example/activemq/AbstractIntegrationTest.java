ccd651b2a6ff9121522f037471b3f565ca8a38e8
alessiozubiani <alessio.zubiani1996@gmail.com>
2024-05-20 18:53:39 +0200

    Aggiunti IntegrationTests

--- src/test/java/com/example/activemq/AbstractIntegrationTest.java
+++ src/test/java/com/example/activemq/AbstractIntegrationTest.java
@@ -11,7 +11,6 @@ import org.testcontainers.utility.DockerImageName;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-
 public abstract class AbstractIntegrationTest {
 	
 	private static final DockerImageName ORACLE_IMAGE = DockerImageName.parse("gvenzl/oracle-free:slim-faststart");
