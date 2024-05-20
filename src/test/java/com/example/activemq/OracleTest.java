5370958e2729805d45c8053c2fb12a88c05a9499
alessiozubiani <alessio.zubiani1996@gmail.com>
2024-05-20 11:41:52 +0200

    Aggiunti IT ed E2E tests

--- src/test/java/com/example/activemq/OracleTest.java
+++ src/test/java/com/example/activemq/OracleTest.java
@@ -49,6 +49,8 @@ public class OracleTest {
  
     @DynamicPropertySource
     private static void setupProperties(DynamicPropertyRegistry registry) {
+    	OracleContainer o = (OracleContainer) oracleContainer;
+    	log.info("JDBC URL: [{}]", o.getJdbcUrl());
     	//jdbc:tc:oracle:thin:localhost:1521/testcontainer
     	String url = "jdbc:tc:oracle:thin:" + oracleContainer.getHost() + ":" + oracleContainer.getMappedPort(1521) + "/testcontainer";
     	registry.add("spring.datasource.url", () -> url);
