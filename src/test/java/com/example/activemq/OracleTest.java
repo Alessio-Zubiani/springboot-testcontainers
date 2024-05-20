bf941d284a24e7eac386214005403cf8841de34a
alessiozubiani <alessio.zubiani1996@gmail.com>
2024-05-20 11:25:26 +0200

    Aggiunti IT ed E2E tests

--- src/test/java/com/example/activemq/OracleTest.java
+++ src/test/java/com/example/activemq/OracleTest.java
@@ -13,9 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.oracle.OracleContainer;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 
 @SpringBootTest
 @Testcontainers
@@ -24,34 +29,38 @@ public class OracleTest {
 	@Autowired
     private DataSource dataSource;
  
-    /*@Container
+    @Container
     private static final GenericContainer<?> oracleContainer = new GenericContainer<>("gvenzl/oracle-free:slim-faststart")
             .withEnv("ORACLE_ROOT_PASSWORD", "pass")
             .withEnv("ORACLE_DATABASE", "testcontainer")
             .withEnv("ORACLE_USER", "user")
             .withEnv("ORACLE_PASSWORD", "pass")
             .withExposedPorts(1521)
-            .waitingFor(Wait.forLogMessage(".*mysqld: ready for connections.*", 2))
-            .withCopyFileToContainer(MountableFile.forClasspathResource("init.sql"), "/docker-entrypoint-initdb.d/schema.sql")
-            ;*/
+            //.waitingFor(Wait.forLogMessage(".*mysqld: ready for connections.*", 2))
+            //.withCopyFileToContainer(MountableFile.forClasspathResource("init.sql"), "/docker-entrypoint-initdb.d/schema.sql")
+            ;
 	
-	@Container
+	/*@Container
 	private static final OracleContainer oracleContainer = new OracleContainer("gvenzl/oracle-free:slim-faststart")
             .withDatabaseName("EMPLOYEE_DB")
             .withUsername("EMPLOYEE_USER")
             .withPassword("EMPLOYEE_PASSWORD")
-            ;
+            ;*/
  
     @DynamicPropertySource
     private static void setupProperties(DynamicPropertyRegistry registry) {
-    	registry.add("spring.datasource.url", () -> oracleContainer.getJdbcUrl());
+    	oracleContainer.getEnv().forEach(e -> {
+    		log.info("Env: [{}]", e);
+    	});
+    	log.info("Host: [{}]", oracleContainer.getHost());
+    	/*registry.add("spring.datasource.url", () -> oracleContainer.getJdbcUrl());
         registry.add("spring.datasource.username", () -> oracleContainer.getUsername());
-        registry.add("spring.datasource.password", () -> oracleContainer.getPassword());
+        registry.add("spring.datasource.password", () -> oracleContainer.getPassword());*/
     }
  
     @Test
     void testTableExists() throws SQLException {
-        try (Connection conn = dataSource.getConnection();
+        try (Connection conn = this.dataSource.getConnection();
             ResultSet resultSet = conn.prepareStatement("SHOW TABLES").executeQuery();) {
             resultSet.next();
  
